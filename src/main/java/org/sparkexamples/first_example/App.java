ff8c5d189a3fae1c34b5d2e58c029ae4bc27222b
DavidChristopher <dc_9683@yahoo.co.id>
2017-11-08 17:09:13 +0700

    deploy test

--- src/main/java/org/sparkexamples/first_example/App.java
+++ src/main/java/org/sparkexamples/first_example/App.java
@@ -1,5 +1,7 @@
 package org.sparkexamples.first_example;
 
+import org.apache.spark.SparkContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public static void startStream(SparkContext sc, int interval) {
+    	System.out.println("DCH----DCH-----DCH");
     }
 }
