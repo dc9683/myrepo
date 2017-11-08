768c66039b48ed7e25048d5387b3330978f37ab4
DavidChristopher <dc_9683@yahoo.co.id>
2017-11-08 16:16:52 +0700

    init

---
+++ src/main/java/org/sparkexamples/first_example/App.java
@@ -0,0 +1,13 @@
+package org.sparkexamples.first_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
